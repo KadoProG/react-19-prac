6fb61f9832e84972bbf11b0db730dcc2def1b23b
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-11-04 10:22:50 +0900

    add: register page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ const Home = () => {
       <p>ようこそ、ホームページへ！</p>
       <nav>
         <Link to="/about">Aboutページへ</Link>
+        <Link to="/register">Registerページへ</Link>
       </nav>
     </div>
   );
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import { Register } from './pages/register';
 
 export const MyRouter = () => {
   return (
@@ -8,6 +9,7 @@ export const MyRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
