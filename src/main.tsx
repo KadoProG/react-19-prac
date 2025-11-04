416fc20075a1aa90ea5eef968d69ef63f65e0fb5
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-11-04 10:25:07 +0900

    add: tailwind css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MyRouter } from './router.tsx';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
