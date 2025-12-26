0a6dfae6dce32b8f649ad9df06e14787d35a3237
KadoProG <sho.kadoguchi@sun-asterisk.com>
2025-12-26 09:34:29 +0900

    update: vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/react-19-prac',
+  server: {
+    host: '0.0.0.0',
+  },
 });
